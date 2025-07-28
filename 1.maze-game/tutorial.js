01e2bb221e13ed92dda6f590ccb552f4c44c3b9a
Shikhar Singh <146508977+spywar18@users.noreply.github.com>
2025-07-28 23:51:17 +0530

    Update tutorial.js

--- 1.maze-game/tutorial.js
+++ 1.maze-game/tutorial.js
@@ -1,11 +1,11 @@
 const prompt = require("prompt-sync")()
 const name = prompt("What is your name? ")
 console.log(`Hello ${name} welcome to our game!`)
-
 const shouldWePlay = prompt("Do you want to play? (y/n)")
 if (shouldWePlay.toLowerCase() === "y") {
 
-//game logic
+
+//game-logic
 const leftOrRight = prompt("You enter a maze, do you want to go left or right?")
 if (leftOrRight === "left") {
     console.log("You go left and see a bridge...")
